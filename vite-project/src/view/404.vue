c3852d0a49a167daac45fbc0a8ee66d1066a6539
ErefIect <luodan_0464@qq.com>
2023-11-01 17:35:19 +0800

    404样式

--- vite-project/src/view/404.vue
+++ vite-project/src/view/404.vue
@@ -4,12 +4,44 @@
 
 <template>
   <div class="not-found">
-    <el-card>
-      <h2>404 Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <el-button type="primary" @click="$router.go(-1)">Go Back</el-button>
+    <el-card class="card">
+      <h2 class="title">404 Not Found</h2>
+      <p class="message">页面找不到了/(ㄒoㄒ)/~~</p>
+<!--      <p class="message">点击按钮原路返回</p>-->
+      <el-button class="button" type="primary" @click="$router.go(-1)">返回</el-button>
     </el-card>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .not-found {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f5f7fa;
+}
+
+    .card {
+        max-width: 400px;
+        padding: 30px;
+        text-align: center;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+    .title {
+        font-size: 24px;
+        color: #333;
+        margin-bottom: 20px;
+}
+
+    .message {
+        margin-bottom: 10px;
+        color: #666;
+}
+
+    .button {
+        width: 100%;
+}
+</style>
