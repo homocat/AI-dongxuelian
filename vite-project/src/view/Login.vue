95c03898441cccd383299b7cae58e64a7b88cfeb
ErefIect <luodan_0464@qq.com>
2023-11-03 14:12:08 +0800

    login update

--- vite-project/src/view/Login.vue
+++ vite-project/src/view/Login.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { reactive, ref } from 'vue'
 
+let isLoginPage = ref(true)
+
 const labelPosition = ref('right')
 
 const formLabelAlign = reactive({
@@ -12,86 +14,119 @@ const formLabelAlign = reactive({
 function handleLogin(username, password) {
 
 }
+
+function jumpRegister() {
+  isLoginPage.value = false
+}
+
+function forgetPasswd() {
+  alert("下次别忘了")
+}
 </script>
 
 <template>
   <div class="box">
-  <div class="login">
-    <h2>AI健康助手</h2>
-    <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" style="max-width: 460px">
-<!--      <el-form-item label="账号">-->
-        <el-input v-model="formLabelAlign.region" placeholder="用户名" class="input"/>
-<!--      </el-form-item>-->
-<!--      <el-form-item label="密码">-->
-        <el-input v-model="formLabelAlign.type" placeholder="密码" class="input"/>
-<!--      </el-form-item>-->
-    </el-form>
+    <div class="login">
+      <!-- 标题 -->
+      <h2>AI健康助手</h2>
 
-     <el-button type="primary" @click="handleLogin" class="button">登录</el-button>
+      <!-- 输入框 -->
+      <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" style="max-width: 460px">
+        <!--      <el-form-item label="账号">-->
+        <el-input v-model="formLabelAlign.region" placeholder="用户名" class="input" />
+        <!--      </el-form-item>-->
+        <!--      <el-form-item label="密码">-->
+        <el-input v-model="formLabelAlign.type" placeholder="密码" class="input" />
+        <el-input v-model="formLabelAlign.type" placeholder="确认密码" class="input" v-if="!isLoginPage"/>
+        <!--      </el-form-item>-->
+      </el-form>
 
-    <div class="register">
-      <span><a href="#">点击注册</a>  <a href="#">忘记密码</a></span>
+      <!-- 登录按钮 -->
+      <el-button type="primary" @click="handleLogin" class="button" v-if="isLoginPage">
+        登录
+      </el-button>
+      <!-- 注册按钮 -->
+      <el-button type="primary" @click="handleLogin" class="button" v-else>
+        注册
+      </el-button>
+      <div class="register" v-if="isLoginPage">
+        <span @click="jumpRegister">
+          点击注册
+        </span>
+        <span @click="forgetPasswd">
+          忘记密码
+        </span>
+      </div>
+      <div v-else>
+        <span @click="isLoginPage=true">
+          已有密码, 点击登录
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-    .box {
-      background-image: url("src/images/bk.jpg");
-      width: 100vw;
-      height: 100vh;
-      background-size: cover;
-      background-position: center;
-    }
+.box {
+  background-image: url("src/images/bk.jpg");
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+}
 
-    .login {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: rgba(255, 255, 255, 0.5);
-        border-radius: 10px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-        text-align: center;
-    }
+.login {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 10px;
+  box-shadow: 10 10 20px rgba(47, 46, 46, 0.2);
+  text-align: center;
+}
 
-    .login h2 {
-        text-align: center;
-        color: #333;
-        font-size: 28px;
-        margin-bottom: 10px;
-    }
+.login h2 {
+  text-align: center;
+  color: #a3c5a1;
+  font-size: 28px;
+  margin-bottom: 10px;
+}
 
-    .login .register {
-        text-align: center;
-        margin-bottom: 20px;
-    }
+.login .register {
+  text-align: center;
+}
 
-    .login .register a {
-        text-decoration: none;
-        color: #4169E1;
-        font-size: 16px;
-        margin-right: 5px;
-    }
+.register {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  justify-content: space-between;
+}
 
-    .login .register span {
-        margin-right: 10px;
-    }
+.login .register span {
+  color: rgb(147, 170, 111);
+  margin: 0 40px;
+}
 
-    .input {
-        align-items: center;
-        margin-bottom: 10px;
-    }
+.login .register span:hover {
+  margin-right: 10px;
+  color: rgb(79, 88, 66);
+  transition: all;
+}
 
-    .button {
-        margin: 10px auto;
-        width: 100%;
-        height: 40px;
-        font-size: 16px;
-    }
+.input {
+  align-items: center;
+  margin-bottom: 10px;
+}
 
+.button {
+  margin: 10px auto;
+  width: 100%;
+  height: 40px;
+  font-size: 16px;
+}
 </style>
