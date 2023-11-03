543b0ecf30b4f26eaeae55c1451ff03dd35230d1
ErefIect <luodan_0464@qq.com>
2023-11-03 11:47:36 +0800

    解决跨域

--- app/main.py
+++ app/main.py
@@ -14,3 +14,11 @@ app = FastAPI()
 
 app.include_router(api_router, prefix="/v1")
 app.mount("/v2", WSGIMiddleware(flask_app))
+# 添加 CORS 中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源的请求
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有 HTTP 方法
+    allow_headers=["*"],  # 允许所有请求头部
+)
